b26441f32010b19502979adc138aa5ffe90fcc5c
Arun Tomar <arun@solutionenterprises.co.in>
2016-03-01 10:48:15 +0530

    start adding node related subcommands

--- main.go
+++ main.go
@@ -5,12 +5,21 @@ import (
 	"fmt"
 	"github.com/go-chef/chef"
 	"io/ioutil"
+	"log"
 	"os"
 )
 
 var (
 	client *chef.Client
 )
+
+// SubCommand struct is to identify different subcommands and run them.
+type SubCommand struct {
+	// name of the subcommand
+	Name string
+	// Description
+	Description string
+}
 
 func init() {
 	//get the chef config from env params
@@ -49,6 +58,37 @@ func main() {
 		os.Exit(1)
 	}
 
+	switch args[0] {
+	case "node":
+		switch args[1] {
+		case "list":
+			NodeList()
+		case "show":
+			NodeShow(args[2])
+		}
+	default:
+		usage()
+	}
+}
+
+// NodeList will fetch and display node list.
+func NodeList() {
+	allNodes, err := client.Nodes.List()
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+	for k := range allNodes {
+		fmt.Println(k)
+	}
+}
+
+// NodeShow will disply the details of the given node.
+func NodeShow(name string) {
+	node, err := client.Nodes.Get(name)
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+	fmt.Printf("Node Name: %s\n Environment: %s\n Run List: %s\n ", node.Name, node.Environment, node.RunList)
 }
 
 func usage() {
